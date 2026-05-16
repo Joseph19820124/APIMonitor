8c1198468e2b86d8885218ac7d01f2fc481dc63e
chen jian <chenjian@chens-MacBook-Air.local>
2026-05-17 04:31:49 +0800

    Show APIMonitor as a regular window app

--- Sources/APIMonitor/App/APIMonitorApp.swift
+++ Sources/APIMonitor/App/APIMonitorApp.swift
@@ -6,7 +6,6 @@ struct APIMonitorApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        // 无主窗口，纯菜单栏 app
         Settings { EmptyView() }
     }
 }
--- Sources/APIMonitor/App/AppDelegate.swift
+++ Sources/APIMonitor/App/AppDelegate.swift
@@ -5,10 +5,11 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem?
     private var popover: NSPopover?
+    private var mainWindow: NSWindow?
     private var viewModel = DashboardViewModel()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.accessory)
+        NSApp.setActivationPolicy(.regular)
 
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let button = statusItem?.button {
@@ -27,11 +28,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         )
         self.popover = popover
 
+        setupMainWindow()
+
         // 启动时刷新所有数据
         Task { await viewModel.refreshAll() }
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
-            self?.showPopover()
+            self?.showMainWindow()
         }
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        showMainWindow()
+        return true
     }
 
     @objc func togglePopover() {
@@ -47,6 +55,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         guard let popover else { return }
         popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
         popover.contentViewController?.view.window?.makeKey()
+        Task { await viewModel.refreshAll() }
+    }
+
+    private func setupMainWindow() {
+        let controller = NSHostingController(
+            rootView: MenuBarView().environmentObject(viewModel)
+        )
+        let window = NSWindow(contentViewController: controller)
+        window.title = "APIMonitor"
+        window.setContentSize(NSSize(width: 380, height: 520))
+        window.styleMask = [.titled, .closable, .miniaturizable, .resizable]
+        window.isReleasedWhenClosed = false
+        window.collectionBehavior = [.fullScreenNone]
+        window.center()
+        mainWindow = window
+    }
+
+    private func showMainWindow() {
+        guard let mainWindow else { return }
+        if !mainWindow.isVisible {
+            mainWindow.center()
+        }
+        mainWindow.makeKeyAndOrderFront(nil)
+        mainWindow.orderFrontRegardless()
+        NSApp.activate(ignoringOtherApps: true)
         Task { await viewModel.refreshAll() }
     }
 }
